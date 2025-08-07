25cc9f5b54b3a174c133856c87c7d356de3f0dc3
Mulky Malikul Dhaher <kymulky@gmail.com>
2025-08-07 12:59:22 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -8,10 +9,10 @@ const firebaseConfig = {
     apiKey: "AIzaSyD0zaikxZy7gMyA0cX-0RcAZGvtnR8eIXo",
     authDomain: "mulkymalikuldhr-s-projects.firebaseapp.com",
     projectId: "mulkymalikuldhr-s-projects",
-    storageBucket: "mulkymalikuldhr-s-projects.appspot.com",
+    storageBucket: "mulkymalikuldhr-s-projects.firebasestorage.app",
     messagingSenderId: "705987898649",
-    appId: "1:705987898649:web:489f82e797a60ba10e2988",
-    measurementId: "G-SF9T87L0KP"
+    appId: "1:705987898649:web:851fc9327c2eac290e2988",
+    measurementId: "G-HX5RWGS5K2"
 };
 
 
@@ -19,5 +20,11 @@ const firebaseConfig = {
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+
+// Initialize Analytics if running in the browser
+if (typeof window !== 'undefined') {
+    getAnalytics(app);
+}
+
 
 export { app, auth, googleProvider };

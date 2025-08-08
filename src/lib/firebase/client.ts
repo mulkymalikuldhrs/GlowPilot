983807f893fc33bac1fc0592ad17cf7811929a57
Mulky Malikul Dhaher <kymulky@gmail.com>
2025-08-08 12:18:12 +0000

    feat: secure firebase credentials

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD0zaikxZy7gMyA0cX-0RcAZGvtnR8eIXo",
-  authDomain: "mulkymalikuldhr-s-projects.firebaseapp.com",
-  projectId: "mulkymalikuldhr-s-projects",
-  storageBucket: "mulkymalikuldhr-s-projects.firebasestorage.app",
-  messagingSenderId: "705987898649",
-  appId: "1:705987898649:web:851fc9327c2eac290e2988",
-  measurementId: "G-HX5RWGS5K2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
